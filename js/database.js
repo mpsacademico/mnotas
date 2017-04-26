31b2a14331729a5aaf904792ac87f733758da599
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-04-26 08:24:11 -0300

    Listagem básica de notas gravadas em lista

--- js/database.js
+++ js/database.js
@@ -17,3 +17,13 @@ function criarNota(titulo, conteudo, visibilidade) {
     };
     return firebase.database().ref().child('notas').push(data);
 }
+
+firebase.database().ref('notas').on('value', function (snapshot) {
+    lista.innerHTML = '';
+    snapshot.forEach(function (item) {
+        var li = document.createElement('li');
+		li.className = "collection-item";
+        li.appendChild(document.createTextNode(item.val().titulo + ' - ' + item.val().conteudo + ' - ' + item.val().visibilidade));
+        lista.appendChild(li);
+    });
+});
