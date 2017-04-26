b54d505b5bf2b933ef5af3231ce68d4a971e0aa7
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-04-26 08:03:11 -0300

    Inclusão dos scripts Firebase no projeto

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var config = {
+apiKey: "AIzaSyA-O5s7rnF_AyNsGTTTp1EHMibfLDER6uU",
+authDomain: "mnotas-33ea3.firebaseapp.com",
+databaseURL: "https://mnotas-33ea3.firebaseio.com",
+projectId: "mnotas-33ea3",
+storageBucket: "mnotas-33ea3.appspot.com",
+messagingSenderId: "346963643539"
+};
+firebase.initializeApp(config);
---
+++ js/database.js
@@ -0,0 +1,8 @@
+var titulo = document.getElementById('titulo');
+var conteudo = document.getElementById('conteudo');
+var visibilidade = document.getElementById('visibilidade');
+var criar = document.getElementById('criar');
+
+criar.addEventListener('click', function () {
+    console.log('criar');
+});
