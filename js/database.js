db4ee52369f6ab87ddad4db688e2240bcc96600a
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-05-02 18:24:40 -0300

    Inserção de eventos para exibição de menus nas notas

--- js/database.js
+++ js/database.js
@@ -9,6 +9,10 @@ var deletarTudo = document.getElementById('deletar-tudo');
 
 var painelnota = document.getElementById('painel-nota');
 
+var menuzinho = function(){
+	//alert("click executado");
+}
+
 criar.addEventListener('click', function () {
     var r = criarNota(titulo.value, conteudo.value, visibilidade.value);
 	painelnota.style.display = 'none';
@@ -43,14 +47,12 @@ firebase.database().ref('notas').on('value', function (snapshot) {
 		var d = dia + '/' + (mes+1) + '/' + ano;
 		var h = hours + ':' + minutes + ':' + seconds;
 		var dh = d + ' às ' + h;
-		lista.innerHTML += '<div class="card"><div class="card-content"><span class="card-title">'+item.val().titulo+'<a href="#"><i class="material-icons right">more_vert</i></a></span><p><small>'+dh+'</small></p><p>'+item.val().conteudo+'</p></div></div>';
-        /*var li = document.createElement('li');		
-		li.className = "collection-item";
-		li.setAttribute("data-key", item.key);
-        li.appendChild(document.createTextNode(item.val().titulo + ' - ' + item.val().conteudo + ' - ' + item.val().visibilidade ));
-		li.innerHTML += " <a href=\"#editar\"><i class=\"material-icons\">edit</i</a><a href=\"#confirmar-deletar\"><i class=\"material-icons\">delete</i</a>";
-        lista.appendChild(li);*/
+		lista.innerHTML += '<div class="card"><div class="card-content"><span class="card-title cardclick">'+item.val().titulo+'</span><p><small>'+dh+'</small></p><p>'+item.val().conteudo+'</p></div></div>';       
     });
+	var cardclick = document.getElementsByClassName('cardclick');
+	for (var i = 0; i < cardclick.length; i++) {
+		cardclick[i].addEventListener('click', menuzinho, false);
+	}
 });
 
 firebase.database().ref('notas').on('value', function (snapshot) {
