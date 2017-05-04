066079e81939bd5a3c8d8f12e34b2a61d533af6f
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-05-04 20:39:23 -0300

    Seleção de elementos do formulário de edição de nota

--- js/database.js
+++ js/database.js
@@ -14,19 +14,22 @@ var menuzinho = function(){
 	var id = tg.getAttribute("data-id");
 	var pe = tg.parentElement;
 	pe = pe.parentElement;
-	var json = JSON.parse(pe.getAttribute("data-json"));
-	console.log(json);
-	pe.setAttribute("class","card-content blue lighten-5");
-	pe.innerHTML = '<span class="card-title">Edição da nota '+id+' <a href="#"><i class="material-icons right">close</i></a></span><input type="text" value="'+json.titulo+'"><textarea class="materialize-textarea">'+json.conteudo+'</textarea><btn class="btn blue" onclick=\'editar("'+id+'")\'>Editar</btn><btn class="btn red" onclick=\'remover("'+id+'")\'>Remover</btn>';
+	var json = JSON.parse(pe.getAttribute("data-json"));	
+	pe.setAttribute("class","nota-"+id+" card-content blue lighten-5");
+	pe.innerHTML = '<span class="card-title">Edição da nota '+id+' <a href="#"><i class="material-icons right">close</i></a></span><input type="text" name="ttitulo" value="'+json.titulo+'"><textarea id="tconteudo" class="materialize-textarea">'+json.conteudo+'</textarea><btn class="btn blue" onclick=\'editar("'+id+'")\'>Editar</btn><btn class="btn red" onclick=\'remover("'+id+'")\'>Remover</btn>';
 }
 
 function remover(id){
 	firebase.database().ref("notas").child(id).remove();
 }
 
-function editar(id){	
+function editar(id){
+	var el = document.getElementsByClassName("nota-"+id)[0];
+	var json = JSON.parse(el.getAttribute("data-json"));
+	var titulo = el.childNodes[1].value;
+	var conteudo = el.childNodes[2].value;
 	var up = {};
-	up["/notas/"+id] = {titulo:"marcos"};
+	up["/notas/"+id] = {titulo:titulo, timestamp:json.timestamp, conteudo:conteudo};
 	return firebase.database().ref().update(up);
 }
 
@@ -66,7 +69,7 @@ firebase.database().ref('notas').on('value', function (snapshot) {
 		var d = dia + '/' + (mes+1) + '/' + ano;
 		var h = hours + ':' + minutes + ':' + seconds;
 		var dh = d + ' às ' + h;
-		lista.innerHTML += '<div class="card"><div class="card-content" data-json=\''+JSON.stringify(item.val())+'\'><span class="card-title" data-id='+item.key+'>'+item.val().titulo+'<a href="#" class="cardclick right" data-id='+item.key+'>(...)</a></span><small>'+dh+'</small><p>'+item.val().conteudo+'</p></div></div>';       
+		lista.innerHTML += '<div class="card"><div class="card-content nota-'+item.key+'" data-json=\''+JSON.stringify(item.val())+'\'><span class="card-title" data-id='+item.key+'>'+item.val().titulo+'<a href="#" class="cardclick right" data-id='+item.key+'>(...)</a></span><small>'+dh+'</small><p>'+item.val().conteudo+'</p></div></div>';       
     });
 	var cardclick = document.getElementsByClassName('cardclick');
 	for (var i = 0; i < cardclick.length; i++) {
