39687db0a4db08dabc0e95c8d5c36ad4219496a2
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-05-20 23:18:26 -0300

    Alteração do estilo do botão de arquivamento

--- js/database.js
+++ js/database.js
@@ -19,9 +19,9 @@ var menuzinho = function(){
 	pe.setAttribute("class","nota-"+id+" card-content blue lighten-5");
 	pe.innerHTML = '<span class="card-title">Edição da nota '+id+' <a onclick="reabrir()"><i class="material-icons right">close</i></a></span>'+
 	'<input type="text" name="ttitulo" value="'+json.titulo+'">'+
-	'<textarea id="tconteudo" class="materialize-textarea">'+json.conteudo+'</textarea>'+
-	'<btn class="btn btn-flat" onclick=\'\'>Arquivar</btn>'+
-	'<btn class="btn btn-flat" onclick=\'remover("'+id+'")\'>Apagar</btn>'+
+	'<textarea id="tconteudo" class="materialize-textarea">'+json.conteudo+'</textarea>'+	
+	'<btn class="btn btn-flat" onclick=\'remover("'+id+'")\'>Apagar</btn> '+
+	'<btn class="btn lime darken-4" onclick=\'\'>Arquivar</btn> '+
 	'<btn class="btn blue" onclick=\'editar("'+id+'")\'>Salvar</btn>';
 	var cardclick = document.getElementsByClassName('cardclick');
 	for (var i = 0; i < cardclick.length; i++) {
